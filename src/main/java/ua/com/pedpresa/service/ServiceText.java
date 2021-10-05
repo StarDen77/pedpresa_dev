25c7d3c03b996e4c1906ee2b73624fdf92197ef3
StarDen77 <starden@ukr.net>
2021-10-05 13:34:10 +0300

    Great commit work (add tags in modificated string)

--- src/main/java/ua/com/pedpresa/service/ServiceText.java
+++ src/main/java/ua/com/pedpresa/service/ServiceText.java
@@ -26,7 +26,7 @@ public class ServiceText {
                     ResultSet.CONCUR_UPDATABLE);
             ResultSet rs = stmt.executeQuery(Props.SQL4);
             int z = 0;
-
+            StringBuilder modPost = new StringBuilder();
             while (rs.next()) {
 
                 news = new News(rs.getInt(1),
@@ -50,94 +50,112 @@ public class ServiceText {
                     String content = Jsoup.clean(news.getContent(), Safelist.relaxed());
 //                    String content = news.getContent().replace("  "," ").trim();
                     String contWithoutRN = content.substring(0, content.contains("<p>Читайте також") ? content.indexOf("<p>Читайте також") : content.length());
-                    System.out.println(contWithoutRN);
+//                    System.out.println(contWithoutRN);
                     boolean endOfHref;
+                    String mod_title = modificate(title, con);
+                    System.out.println(mod_title);
+                    System.out.println("");
+
+//                    List<String> sentences = new ArrayList<>();
+                    List<String> words;
+                    Map<String, Integer> countWords = new HashMap<>();
+
+                    modPost.append(mod_title).append(System.lineSeparator());
+//                    sentences = List.of(Jsoup.parse(contWithoutRN).text().split("\\. "));
+
+
                     List<String> strings = new ArrayList<>(List.of(contWithoutRN.split("\\n")));
 
                     for (String string : strings) {
-                        string = string.replace("<"," <");
-                        string = string.replace(">","> ");
-                        string = string.replace("&nbsp;","");
-                        string = string.replace("  "," ").trim();
-                        System.out.println(StringUtils.countOccurrencesOf(string," "));
+                        string = string.replace("<", " <");
+                        string = string.replace(">", "> ");
+                        string = string.replace("&nbsp;", " ");
+                        string = string.replace("  ", " ").trim();
+//                        System.out.println(StringUtils.countOccurrencesOf(string," "));
                         StringBuilder strA = new StringBuilder();
                         List<String> allWords = List.of(string.split(" "));
-                        strA.append(allWords.size());
+//                        strA.append(allWords.size() - 1).append(";");
+                        String tString = Jsoup.parse(string).text().trim();
+                        String mod_string = tString.isEmpty() ? "" :
+                        modificate(tString.replace(" ,", ","), con).replace("..", ".");
+//                        String mod_string = modificate(string, con).replace("..", ".");
+
+                        int u = tString.length() - mod_string.length();             // variable = different lengths between original string and modificated string
                         for (int i = 0; i < allWords.size(); i++) {
-                            endOfHref  = true;
-//                            System.out.println(allWords.get(i));
-                            if(allWords.get(i).startsWith("<") && allWords.get(i).endsWith(">")) {
-                                strA.append(allWords.get(i)).append(i).append(";");
+                            endOfHref = true;
+                            if (allWords.get(i).startsWith("<") && allWords.get(i).endsWith(">")) {
+                                strA.append(allWords.get(i)).append(i==0? i : i - u).append(";");
+                                u++;
                             }
-                            if(allWords.get(i).startsWith("<a") && allWords.get(i+1).startsWith("href")) {
-                                int q = 0;
-                                while (endOfHref) {
-                                    q++;
-//                                    System.out.println(allWords.get(i+q)+" "+q);
-                                    if (allWords.get(i+q).endsWith("/a>")) {
-                                        strA.append(allWords.get(i))
-                                                .append(" ")
-                                                .append(allWords.get(i+1))
-                                                .append(i).append("-")
-                                                .append(allWords.get(i+q))
-                                                .append(i+q);
-                                        endOfHref = false;
-                                    }
-                                }
+                            if (allWords.get(i).startsWith("<a") && allWords.get(i + 1).startsWith("href")) {
+                                strA.append(allWords.get(i)).append(" ").append(allWords.get(i + 1)).append(i - u).append(";");
+                                u = u + 2;
                             }
                         }
-                        System.out.println(strA.toString());
 
-                        System.out.println("|"+string+"|");
+//                        ------------------ return tags to string ------------
+                        System.out.println("|" + string + "|");
+                        List<String> strTag = List.of(strA.toString().split(";"));
+                        mod_string = mod_string + (" ".repeat(strTag.size()));
+                        System.out.println("|" + mod_string + "|");
+
+                        String tagS = "";
+                        String tag = "";
+                        int tagPos1 = 0;
+                        int tagPos2 = 0;
+                        System.out.println(strA);
+                        StringBuilder sb = new StringBuilder();
+
+
+                        List<String> modStringList = List.of(mod_string.split(" "));
+
+                        for (int i = 0; i < strTag.size(); i++) {
+                            tag = strTag.get(i);
+                            tagPos1 = Integer.parseInt(tag.substring(tag.indexOf(">") + 1));
+                            if ((i + 1) >= strTag.size()) {
+                                tagPos2 = Integer.parseInt(strTag.get(i).substring(strTag.get(i).indexOf(">") + 1));
+                            } else {
+                                tagPos2 = Integer.parseInt(strTag.get(i + 1).substring(strTag.get(i + 1).indexOf(">") + 1));
+                            }
+                            if (tagPos2 > modStringList.size()) tagPos2 = modStringList.size();
+                            tagS = tag.substring(0, tag.indexOf(">") + 1);
+
+
+                            if (tagPos1 <= 0) tagPos1 = 0;
+
+                            sb.append(tagS);
+                            for (int j = tagPos1; j < tagPos2; j++) {
+                                sb.append(modStringList.get(j)).append(" ");
+                            }
+
+                        }
+
+                        System.out.println("+|" + sb + "|+");
+                        modPost =  modPost.append(sb).append(System.lineSeparator());
+//                        System.out.println(strA);
+
                         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~");
+                        System.out.println();
                     }
 
 // считаем количество слов всего + первый тег + позиция тега + ";" и т.д.
 
-                    System.out.println(z);
-                    System.out.println(title);
-
-
-                    String titleMod = "";
-                    String textMod = "";
-// ------------------------------------ parsing sentences -------------------------------------------
-                    List<String> sentences = new ArrayList<>();
-                    List<String> words = new ArrayList<>();
-                    Map<String, Integer> countWords = new HashMap<>();
-
-
-                    String mod_title = modificate(title, con);
-                    System.out.println(mod_title);
-                    System.out.println("");
-                    System.out.println("");
-
-                    StringBuilder modPost = new StringBuilder();
-                    modPost.append(mod_title).append("\\n");
-                    sentences = List.of(Jsoup.parse(contWithoutRN).text().split("\\. "));
-                    for (String sentence : sentences) {
-                        System.out.println(sentence);
-
-                        String mod_sentence = modificate(sentence, con).replace("..", ".");
-
-                        System.out.println(mod_sentence);
-                        modPost.append(mod_sentence).append("\\n");
-
-                        System.out.println("+++++++++++++++++++++++++++++++");
-                    }
-
                     //                    ---------------------------- list of words -----
+                    modPost.append(System.lineSeparator()).append("Tags:");
+
                     words = List.of(modPost.toString().split(" "));
                     for (String word : words) {
                         word = fSym(word).isEmpty() ? word : word.substring(1);
                         word = lastSym(word).isEmpty() ? word : word.substring(0, word.length() - 1);
-//                        System.out.println(word);
                         if (countWords.containsKey(word)) countWords.replace(word, countWords.get(word) + 1);
                         else countWords.put(word, 1);
                     }
 
                     for (Map.Entry<String, Integer> entry : countWords.entrySet()) {
-                        if (entry.getValue() > 2 && entry.getKey().length() > 2)
-                            System.out.println(entry.getKey() + " == " + entry.getValue());
+                        if (entry.getValue() > 2 && entry.getKey().length() > 2) {
+//                            System.out.println(entry.getKey() + " == " + entry.getValue());       !!!!!!!!!!! добавить сверку с базой по ключевым словам !!!!!!!!!!!!
+                            modPost.append(" #").append(entry.getKey());
+                        }
                     }
 
 //                  ----------------------- end list of words
@@ -146,15 +164,19 @@ public class ServiceText {
                 } // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! end of EXECUTOR
 
                 System.out.println("----------------------------------------------------");
+                System.out.println();
 
 
 // ----------------------------- end of parsing sentences ------------------------------------
             }
+            System.out.println(modPost);
+
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     private static String modificate(String sent, Connection con) throws SQLException {
@@ -173,21 +195,15 @@ public class ServiceText {
         String fW = "";
         String fS = "";
 
-
-//        for (int i = 0; i < words.length; i++) {
-//            System.out.println(words[i]);
-//        }
-
-
         for (int i = 0; i < words.length; i++) {
             mod_word = "";
             rm = 5;
             lastS = "";
             fS = "";
 
-            if (words[i].charAt(0) == '“') {
-
-            }
+//            if (words[i].charAt(0) == '“') {
+//
+//            }
 //            if(words[i].contains("<")) i++;
 
 
@@ -296,13 +312,21 @@ public class ServiceText {
             else resultString.append(mod_word).append(" ");
 
         }
-        String result = resultString.toString().trim() + ". ";
-
+        String result = "";
+        if (resultString.toString().trim().endsWith(":") ||
+                resultString.toString().trim().endsWith(";") ||
+                resultString.toString().trim().endsWith("!") ||
+                resultString.toString().trim().endsWith("?")) {
+            result = resultString.toString().trim() + " ";
+        } else {
+            result = resultString.toString().trim() + ". ";
+        }
         return result.substring(0, 1).toUpperCase() + result.substring(1);
     }
 
     public static String lastSym(String str) {
         String res = "";
+        if (str.isEmpty()) return res;
         char c = str.charAt(str.length() - 1);
         if (PUNCT.indexOf(c) >= 0) {
             res = str.substring(str.length() - 1);
@@ -313,11 +337,10 @@ public class ServiceText {
 
     public static String fSym(String str) {
         String res = "";
+        if (str.isEmpty()) return res;
         char c = str.charAt(0);
-//        System.out.println("--"+c+"--");
         if (PUNCT.indexOf(c) >= 0) {
             res = str.substring(0, 1);
-//            System.out.println(str + " "+ res);
         }
 
         return res;
